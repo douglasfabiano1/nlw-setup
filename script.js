fad87abd0b5c472b04255ce6f99543c9957ba00c
Douglas Fabiano <douglasfabiano23@gmail.com>
2023-01-20 19:57:30 -0300

    add readme

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const form = document.querySelector('form') 
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ['01-01', '01-02', '01-06', '01-09', '01-12', '01-15'],
-  takePills: ['01-03'],
-  journal: ['01-02']
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add(){
+
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  //const today = "12/01"
+  const dayExists = nlwSetup.dayExists(today)
+  
+  if(dayExists){
+    alert("Dia já incluso 🔴")
+    return
+  }
+  
+  alert("Adicionado com sucesso ✔")
+  nlwSetup.addDay(today) 
 }
 
+function save(){
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
